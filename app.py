20e915e64c94ee0b189484c6c7c6a6fb9e391e3f
ivan <ivan.strutynskyi.22@pnu.edu.ua>
2024-10-26 14:11:27 +0300

    новив app.py та base.html

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return redirect(url_for('resume'))
+
 @app.route('/resume')
 def resume():
-    return render_template('resume.html')
+    return render_template('resume.html', title='Резюме Івана Струтинського')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
