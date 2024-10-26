6d38de6fb7c0bb30911663852d2cd6d6c143c00f
ivan <ivan.strutynskyi.22@pnu.edu.ua>
2024-10-26 13:05:16 +0300

    одані нові файли для резюме

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Hello, world!'
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title='Резюме Івана Струтинського')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
